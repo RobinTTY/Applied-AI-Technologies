bc0d84b2a383cc484e1a50b1627768ef3f8e8d1d
SWTM-TheM <kleinweich98@outlook.de>
2020-06-27 12:35:46 +0200

    Added function to convert images to a specific format

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from classes import FullImage
+from PIL import Image
 
-full = FullImage("test.png")
-full.find_post_its()
-full.print_info()
+
+def convert_image(file_path):
+    img = Image.open(file_path)
+    factor = 128 / img.size[0] if abs(1 - (128 / img.size[0])) > abs(1 - (32 / img.size[1])) else 32 / img.size[1]
+    img = img.resize((int(img.size[0] * factor), int(img.size[1] * factor))).convert("L")
+    bg = Image.new('RGBA', (128, 32), (255, 255, 255, 255))
+    bg.paste(img, (0, 0))
+    return bg
+
+
+res = convert_image("test.png")
+res.show()
+# res.save("output.png")
