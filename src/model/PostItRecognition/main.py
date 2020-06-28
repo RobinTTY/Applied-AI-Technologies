540d6a92081dfe2e2697d3dd666751131aa757e5
SWTM-TheM <kleinweich98@outlook.de>
2020-06-28 09:22:39 +0200

    Convert function now actually works

--- src/model/PostItRecognition/main.py
+++ src/model/PostItRecognition/main.py
@@ -3,8 +3,9 @@ from PIL import Image
 
 def convert_image(file_path):
     img = Image.open(file_path)
-    factor = 128 / img.size[0] if abs(1 - (128 / img.size[0])) > abs(1 - (32 / img.size[1])) else 32 / img.size[1]
-    img = img.resize((int(img.size[0] * factor), int(img.size[1] * factor))).convert("L")
+    img = img.convert("L").resize((128, int(img.size[1] * (128 / img.size[0]))))
+    if img.size[1] > 32:
+        img = img.resize((int(img.size[0] * (32 / img.size[1])), 32))
     bg = Image.new('RGBA', (128, 32), (255, 255, 255, 255))
     bg.paste(img, (0, 0))
     return bg
