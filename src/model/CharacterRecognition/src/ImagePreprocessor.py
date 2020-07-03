7f1516a3bb6ade65a62d1556f10867dac65ed27d
Robin_tty <muellerobin95@gmail.com>
2020-07-03 11:10:33 +0200

    add more training data

--- src/model/CharacterRecognition/src/ImagePreprocessor.py
+++ src/model/CharacterRecognition/src/ImagePreprocessor.py
@@ -13,6 +13,7 @@ class ImagePreprocessor:
         img = img.convert("L").resize((128, int(img.size[1] * (128 / img.size[0]))))
         if img.size[1] > 32:
             img = img.resize((int(img.size[0] * (32 / img.size[1])), 32))
+        img = Image.fromarray(np.where(np.array(img) > np.array(img)[0][0] - 20, 255, 0))
         bg = Image.new('RGBA', (128, 32), (255, 255, 255, 255))
         bg.paste(img, (0, 0))
         return bg
--- src/model/CharacterRecognition/src/main.py
+++ src/model/CharacterRecognition/src/main.py
@@ -9,34 +9,34 @@ from Model import Model, DecoderType
 
 
 class FilePaths:
-	"""relevant file paths"""
-	fnCharList = '../model/charList.txt'
-	fnAccuracy = '../model/accuracy.txt'
-	fnInfer = '../data/Smartphone_color.jpg'
+    """relevant file paths"""
+    fnCharList = '../model/charList.txt'
+    fnAccuracy = '../model/accuracy.txt'
+    fnInfer = '../data/output.png'
 
 
 def extract_text(model, input_img):
-	"""recognize text in image provided by file path"""
-	img = ImagePreprocessor.preprocess(cv2.imread(input_img, cv2.IMREAD_GRAYSCALE), Model.imgSize)
-	batch = Batch(None, [img])
-	(recognized, probability) = model.infer_batch(batch, True)
-	print('Recognized:', '"' + recognized[0] + '"')
-	print('Probability:', probability[0])
+    """recognize text in image provided by file path"""
+    img = ImagePreprocessor.preprocess(cv2.imread(input_img, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+    batch = Batch(None, [img])
+    (recognized, probability) = model.infer_batch(batch, True)
+    print('Recognized:', '"' + recognized[0] + '"')
+    print('Probability:', probability[0])
 
 
 def main():
-	# TODO: check if needed
-	tf.compat.v1.disable_eager_execution()
-	decoder_type = DecoderType.BestPath
+    # TODO: check if needed
+    tf.compat.v1.disable_eager_execution()
+    decoder_type = DecoderType.BestPath
 
-	# infer text on test image
-	print(open(FilePaths.fnAccuracy).read())
-	model = Model(open(FilePaths.fnCharList).read(), decoder_type, must_restore=True)
-	extract_text(model, FilePaths.fnInfer)
+    # infer text on test image
+    print(open(FilePaths.fnAccuracy).read())
+    model = Model(open(FilePaths.fnCharList).read(), decoder_type, must_restore=True)
+    extract_text(model, FilePaths.fnInfer)
 
-	# res = ImagePreprocessor.convert_image("../data/test3.png")
-	# res.save("../data/output.png")
+    res = ImagePreprocessor.convert_image("../data/colored/Hochschule_blue.jpg")
+    res.save("../data/output.png")
 
 
 if __name__ == '__main__':
-	main()
+    main()
