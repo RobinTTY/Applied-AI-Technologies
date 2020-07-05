8aaa245fa8a24aeb770026618cd0c779b2aad4c3
Robin_tty <muellerobin95@gmail.com>
2020-07-05 12:40:43 +0200

    fix inspections

--- src/model/WordSegmentation/src/main.py
+++ src/model/WordSegmentation/src/main.py
@@ -7,12 +7,12 @@ def main():
 	"""reads images from data/ and outputs the word-segmentation to out/"""
 
 	# read input images from 'in' directory
-	imgFiles = os.listdir('../data/')
-	for (i,f) in enumerate(imgFiles):
-		print('Segmenting words of sample %s'%f)
+	img_files = os.listdir('../data/')
+	for (i, f) in enumerate(img_files):
+		print('Segmenting words of sample %s' % f)
 		
 		# read image, prepare it by resizing it to fixed height and converting it to grayscale
-		img = prepareImg(cv2.imread('../data/%s'%f), 50)
+		img = prepareImg(cv2.imread('../data/%s' % f), 50)
 		
 		# execute segmentation with given parameters
 		# -kernelSize: size of filter kernel (odd integer)
@@ -22,21 +22,21 @@ def main():
 		res = wordSegmentation(img, kernelSize=25, sigma=11, theta=7, minArea=100)
 		
 		# write output to 'out/inputFileName' directory
-		if not os.path.exists('../out/%s'%f):
-			os.mkdir('../out/%s'%f)
+		if not os.path.exists('../out/%s' % f):
+			os.mkdir('../out/%s' % f)
 		
 		# iterate over all segmented words
-		print('Segmented into %d words'%len(res))
+		print('Segmented into %d words' % len(res))
 		for (j, w) in enumerate(res):
 			(wordBox, wordImg) = w
 			(x, y, w, h) = wordBox
 			# save word
-			cv2.imwrite('../out/%s/%d.png'%(f, j), wordImg)
+			cv2.imwrite('../out/%s/%d.png' % (f, j), wordImg)
 			# draw bounding box in summary image
 			cv2.rectangle(img, (x, y), (x+w, y+h), 0, 1)
 		
 		# output summary image with bounding boxes around words
-		cv2.imwrite('../out/%s/summary.png'%f, img)
+		cv2.imwrite('../out/%s/summary.png' % f, img)
 
 
 if __name__ == '__main__':
