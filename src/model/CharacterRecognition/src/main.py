9639a2e60f92cc974df828ab7fa5cb08d12fe4a1
Robin_tty <muellerobin95@gmail.com>
2020-07-09 21:39:17 +0200

    convert to class

--- src/model/CharacterRecognition/src/main.py
+++ src/model/CharacterRecognition/src/main.py
@@ -5,7 +5,7 @@ import cv2
 import tensorflow as tf
 from ImagePrePreprocessor import ImagePrePreprocessor
 from ImagePreprocessor import ImagePreprocessor
-from DataLoader import DataLoader, Batch
+from DataLoader import Batch
 from Model import Model, DecoderType
 
 
@@ -16,38 +16,46 @@ class FilePaths:
     fnInfer = '../data/output.png'
 
 
-def extract_text(model, input_img):
-    """recognize text in image provided by file path"""
-    img = ImagePreprocessor.preprocess(cv2.imread(input_img, cv2.IMREAD_GRAYSCALE), Model.imgSize)
-    batch = Batch(None, [img])
-    (recognized, probability) = model.infer_batch(batch, True)
-    #print('Recognized:', '"' + recognized[0] + '"')
-    #print('Probability:', probability[0])
-    return recognized[0]
+class PostItExtractor:
+    def __init__(self):
+        tf.compat.v1.disable_eager_execution()
+        self.decoder_type = DecoderType.BestPath
+        self.model = Model(open(FilePaths.fnCharList).read(), self.decoder_type, must_restore=True)
+
+    def process_image(self, image):
+        # TODO: pass image properly
+
+        # preprocess images
+        file_path = "../data/colored/MultiplePostIts.png"
+        obj = ImagePrePreprocessor(file_path)
+        obj.find_post_its()
+
+        for x in range(len(obj.post_its)):
+            img = ImagePreprocessor.convert_image(obj.post_its[x].file_path)
+            img.save(f"../data/output_{x}.png")
+
+        # infer text on test image
+        print(open(FilePaths.fnAccuracy).read())
+
+        for x in range(len(obj.post_its)):
+            obj.post_its[x].text = self.extract_text(self.model, obj.post_its[x].file_path)
+
+        obj.print_info()
+
+    @staticmethod
+    def extract_text(model, input_img):
+        """recognize text in image provided by file path"""
+        img = ImagePreprocessor.preprocess(cv2.imread(input_img, cv2.IMREAD_GRAYSCALE), Model.imgSize)
+        batch = Batch(None, [img])
+        (recognized, probability) = model.infer_batch(batch, True)
+        print('Recognized:', '"' + recognized[0] + '"')
+        print('Probability:', probability[0])
+        return recognized[0]
 
 
 def main():
-    # TODO: check if needed
-    tf.compat.v1.disable_eager_execution()
-    decoder_type = DecoderType.BestPath
-
-    # preprocess images
-    file_path = "../data/colored/MultiplePostIts.png"
-    obj = ImagePrePreprocessor(file_path)
-    obj.find_post_its()
-
-    for x in range(len(obj.post_its)):
-        img = ImagePreprocessor.convert_image(obj.post_its[x].file_path)
-        img.save(f"../data/output_{x}.png")
-
-    # infer text on test image
-    print(open(FilePaths.fnAccuracy).read())
-    model = Model(open(FilePaths.fnCharList).read(), decoder_type, must_restore=True)
-
-    for x in range(len(obj.post_its)):
-        obj.post_its[x].text = extract_text(model, obj.post_its[x].file_path)
-
-    obj.print_info()
+    extractor = PostItExtractor()
+    extractor.process_image(123)
 
 
 if __name__ == '__main__':
