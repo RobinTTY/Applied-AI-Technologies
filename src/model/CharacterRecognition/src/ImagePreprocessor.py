35ec7f95ffe177bcc7cce6f98903e8646ac612f1
SWTM-TheM <kleinweich98@outlook.de>
2020-07-11 13:27:58 +0200

    Improved post-it recognition

--- src/model/CharacterRecognition/src/ImagePreprocessor.py
+++ src/model/CharacterRecognition/src/ImagePreprocessor.py
@@ -11,9 +11,12 @@ class ImagePreprocessor:
     def convert_image(file_path):
         img = Image.open(file_path)
         img = img.convert("L").resize((128, int(img.size[1] * (128 / img.size[0]))))
+
         if img.size[1] > 32:
             img = img.resize((int(img.size[0] * (32 / img.size[1])), 32))
-        img = Image.fromarray(np.where(np.array(img) > np.array(img)[0][0] - 20, 255, 0))
+
+        average = np.average(np.array(img))
+        img = Image.fromarray(np.where(np.array(img) > 0.9 * average, 255, 0))
         bg = Image.new('RGBA', (128, 32), (255, 255, 255, 255))
         bg.paste(img, (0, 0))
         return bg
