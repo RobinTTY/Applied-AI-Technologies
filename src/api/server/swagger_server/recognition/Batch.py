90b0debf16a9e3940d190d60ef60557dc76b7dc0
Robin_tty <muellerobin95@gmail.com>
2020-07-24 17:13:24 +0200

    remove duplicate

--- src/api/server/swagger_server/recognition/Batch.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-class Batch:
-    """batch containing images and ground truth texts"""
-
-    def __init__(self, gt_texts, images):
-        self.images = np.stack(images, axis=0)
-        self.gtTexts = gt_texts
